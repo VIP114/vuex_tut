07532c4368f5e1c4d027ccd59239e27baef5c716
patelvishv114 <patelvishv343@gmail.com>
2022-02-23 11:05:08 +0530

    state & mutations

---
+++ src/components/Counter.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h1>df</h1>
+        <h1 class="counter">{{counter}}</h1>
+        <button @click="sutractFromCounter(value)">-</button>
+        <input type="number" v-model="value" />
+        <button @click="addToCounter(value)">+</button>
+    </div>
+</template>
+<script>
+import {mapState,mapMutations} from 'vuex'
+export default {
+    name:"Counter",
+    data() {
+        return {
+            value:1,
+            
+        }
+    },
+    computed: {
+        ...mapState(["counter","counter2"])
+    },
+    methods: {
+        ...mapMutations(['addToCounter','sutractFromCounter'])
+    },
+}
+</script>
+<style scoped>
+.counter{
+    font-size: 3rem;
+}
+button{
+    border-radius: 100%  ;
+    border: none;
+    width: 2rem;
+    height: 2rem;
+    cursor: pointer;
+    background-color: rgb(183, 183, 238);
+    margin: 2px;
+}
+input{
+    text-align: center ;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,24 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import {createStore} from 'vuex'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+const store = createStore({
+    state(){
+        return{
+            counter:1, 
+            counter2:2, 
+            counter3:3, 
+        }
+    },
+    mutations:{
+        addToCounter(state,payload){
+            state.counter  = state.counter + payload
+        },
+        sutractFromCounter(state,payload){
+            state.counter =state.counter - payload
+        }
+    }
+})
+const app=createApp(App)
+app.use(store)
+app.mount('#app')
